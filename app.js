193f997838294a53698cd5554ccb8b5ec18d8859
salesforce <salesforce@df13-ltm4.internal.salesforce.com>
2013-10-23 14:46:21 -0700

    add node version to package.json

--- app.js
+++ app.js
@@ -40,7 +40,6 @@ if ('development' == app.get('env')) {
 
 app.get('/', function(req, res) {
 	pgclient.query('SELECT * FROM products', function(err, result) {
-    	done();
     	if(err) return console.error(err);
     	console.log(result.rows);
 		res.render('index', { title: 'Express', products: result.rows });
